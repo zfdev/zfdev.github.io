8b0dab74b5fb199e6079ae3630071b8480ab60ac
JasonZhangX <v-zhlong@microsoft.com>
2020-02-11 19:18:39 +0800

    Add dynamic password.

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const qs = function(selector) {
 const body = qs('body');
 const footer = qs('footer');
 const content = qs('.main-content');
+const PASSWORD_BASE = 'jasonSurvival';
 
+const checkPassRecord = function() {
+    return localStorage.getItem('isPass');
+}
 
 class Page {
     hide() {
@@ -25,13 +29,18 @@ class Page {
 }
 
 const page = new Page();
-
-page.hide();
+const today = new Date();
+const dynamicPassword = function() {
+    const month = (today.getMonth() + 1) > 9 ? today.getMonth() + 1 : '0' + (today.getMonth() + 1);
+    const date = today.getDate() > 9 ? today.getDate() : '0' + today.getDate();
+    return PASSWORD_BASE + month + date;
+}
 
 class Dialog {
     constructor(config = {}) {
         this.config = config;
         this.check = this.check.bind(this);
+        this.clear = this.clear.bind(this);
         this.init();
     }
     init() {
@@ -46,14 +55,21 @@ class Dialog {
                     <h3>Please enter the password:</h3>
                     <input type="text" placeholder="password">
                     <button>Enter</button>
+                    <div class="message"></div>
                 </div>
             </div>
         `;
     }
     check() {
-        if (this.input.value === 'jason') {
+        if (this.input.value && (this.input.value.trim() === dynamicPassword())) {
             this.hide();
+            localStorage.setItem('isPass', true);
+        } else {
+            this.message.textContent = 'The password is incorrect!';
         }
+    }
+    clear() {
+        this.message.textContent = '';
     }
     render() {
         body.insertAdjacentHTML('beforeend', this.template());
@@ -61,12 +77,14 @@ class Dialog {
         this.content = qs('.dialog-container .content');
         this.button = qs('.dialog-container .content button');
         this.input = qs('.dialog-container .content input');
+        this.message = qs('.dialog-container .content .message');
 
         this.content.style.width = this.config.width + 'px';
-        // this.content.style.height = this.config.height + 'px';
+        this.content.style.height = this.config.height + 'px';
     }
     bindEvent() {
         this.button.addEventListener('click', this.check);
+        this.input.addEventListener('focus', this.clear);
     }
     show() {
 
@@ -77,7 +95,10 @@ class Dialog {
     }
 }
 
-const dialog = new Dialog({
-    width: 240,
-    // height: 80,
-});
+if (!checkPassRecord()) {
+    page.hide();
+    const dialog = new Dialog({
+        width: 240,
+        height: 80,
+    });
+}
